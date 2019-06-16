4c1bb35f83e5f519736865fe2f1d4b61787e5ea3
Sammy D <sammydowds1993@gmail.com>
2019-06-16 12:55:52 -0500

    Updated with login routes in settings.py

--- acieta/settings.py
+++ acieta/settings.py
@@ -114,6 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = '/projects/accounts/login'
+LOGIN_REDIRECT_URL = '/projects'
+LOGOUT_REDIRECT_URL = '/projects/accounts/login'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
