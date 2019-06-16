3676d77f3b46ee00e9805763009d52ce44538959
Sammy D <sammydowds1993@gmail.com>
2019-06-16 12:09:52 -0500

    Updated URLs for in both urls.py files

---
+++ projects/urls.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import urls
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name = "index"),
+    path('projectcards/', views.projectcards, name = "projectcards"),
+    path('myprojects/', views.myprojects, name = "myprojects"),
+    path('myprojectcards/', views.myprojectcards, name = "myprojectcards"),
+    path('create/', views.create, name = "create"),
+    path('switch/', views.switch, name = "switch"),
+    path('update/<int:num>/', views.update, name = "update"),
+    path('close/<int:num>/', views.close, name = "close"),
+    path('dashboard/<int:num>/', views.dashboard, name = "dashboard"),
+    path('milestonecomplete/', views.milestonescomplete, name = "milestonecomplete"),
+    path('capacity/', views.capacity, name = "capacity"),
+    path('tasks/', views.tasks, name = "tasks"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', views.register, name = "register"),
+    path('blueprint/<int:num>/', views.blueprint, name = "blueprint"),
+    path('mymonth/', views.mymonth, name = "mymonth")
+
+
+]
