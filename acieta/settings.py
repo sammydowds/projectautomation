c797e8a56a92c72f8609dcfe75cc3b3eb8e5c609
Sammy D <sammydowds1993@gmail.com>
2019-07-09 19:21:48 -0500

    Updated settings.py

--- acieta/settings.py
+++ acieta/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites'
+    
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
