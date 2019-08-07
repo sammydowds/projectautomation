e5e3d1c2fe54d63f884b5c0715708c3c82e61931
Sammy D <sammydowds1993@gmail.com>
2019-08-07 06:40:51 -0500

    Added next milestone column

--- acieta/settings.py
+++ acieta/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b*8j7elcs^i1$sc0@2da3*)%^dfc$!udmf(d_q*l3s^64g2hl1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
